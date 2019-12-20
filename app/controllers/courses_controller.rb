7a9a78f9ce04b81cbeea997f7d381d8adc16af1c
Rails Course <muraokan@oregonstate.edu>
2019-12-20 13:38:53 +0900

    course/show.html refactored for displaying reviews_content

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,14 +1,12 @@
 class CoursesController < ApplicationController
-    before_action :set_course, only: [:show, :destroy]
+    before_action :set_course, only: [:show, :destroy, :update, :edit]
 
     def index
         @courses = Course.all
-        #@reviews = Review.all
     end
     
     def new
         @course = Course.new
-        @course.reviews.build
     end
 
     def create
@@ -22,20 +20,8 @@ class CoursesController < ApplicationController
         end
     end
 
-    def new_reviews
-        @course = Course.new
-        @course.reviews.build
-    end
-
-    def create_reviews
-        @course.reviews.build
-        @course.reviews.save
-    end
-
     def show
-        @reviews = Review.where(course_id: params[:id])
-        #payment_method = params[:payment][:payment_method]
-        #reviewsC = params[:reviews][:reviews_content]
+        
     end
 
     def destroy
@@ -46,19 +32,8 @@ class CoursesController < ApplicationController
         redirect_to courses_path
     end
 
-    # Actually works as create new reviews and keeps current column except for reviews_content
     def edit
-=begin
-        course = Course.find(params[:id])
-        reviews = Review.where(course_id: params[:id])
-        @course = Course.new(coursename: course.coursename, course_content: course.course_content, prerequisite: course.prerequisite, proctoredexams: course.proctoredexams, groupwork: course.groupwork, textbook: course.textbook, reviews_attributes: reviews.reviews_content)
-        @course.save
-        @course.reviews.build
-=end
-        course = Course.find(params[:id])
-        @course = Course.new(coursename: course.coursename, course_content: course.course_content, prerequisite: course.prerequisite, proctoredexams: course.proctoredexams, groupwork: course.groupwork, textbook: course.textbook)
-        @course.save
-        @course.reviews.new
+
     end
 
     def update
@@ -77,10 +52,14 @@ class CoursesController < ApplicationController
     end
 
     def course_params
-        params.require(:course).permit(:coursename, :course_content, :prerequisite, :proctoredexams, :groupwork, :textbook, reviews_attributes: [:reviews_content])
+        params.require(:course).permit(
+            :coursename, 
+            :course_content, 
+            :prerequisite, 
+            :proctoredexams, 
+            :groupwork, 
+            :textbook, 
+            reviews_attributes: [:reviews_content]
+        )
     end 
-
-    def review_params
-        params.require(:create_reviews).permit(reviews_attributes: [:reviews_content])
-    end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,11 @@
 class Course < ApplicationRecord
     # courses are many side of one-to-many associations
     has_many :reviews, dependent: :destroy
-    accepts_nested_attributes_for :reviews
+
+    accepts_nested_attributes_for :reviews,
+                                   allow_destroy: true,
+                                   reject_if: lambda {|attributes| attributes['reviews_content'].blank?}
+
     # ex, CS160, the first CS class at OSU 
     validates :coursename, presence: true, length: {minimum: 5, maximum: 50}
     validates :course_content, presence: true, length: {minimum: 10, maximum: 500}  
