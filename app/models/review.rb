ac24d35b43c75d0c7aa1cd128e69c5850784eff3
Rails Course <muraokan@oregonstate.edu>
2019-12-20 17:44:32 +0900

    Validations for reviews content has increased

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
     # rails5から、belongs_toに対して、required:trueがデフォルトで設定されているからみたいです。
     belongs_to :course
-    validates :reviews_content, presence: true, length: {minimum: 3, maximum: 500}
+    validates :reviews_content, presence: true, length: {minimum: 3, maximum: 2000}
     #validates :course_id, presence: true
 end
