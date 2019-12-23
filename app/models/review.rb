d84cda207cbb858165e1e63ebb0b5bcad7474a61
Rails Course <muraokan@oregonstate.edu>
2019-12-23 15:12:02 +0900

    validations for difficulty and reveiws_content added into reviews.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
     # rails5から、belongs_toに対して、required:trueがデフォルトで設定されているからみたいです。
     belongs_to :course
-    validates :reviews_content, presence: true, length: {minimum: 3, maximum: 2000}
+    validates :reviews_content, presence: true, length: {minimum: 10, maximum: 2000}
+    validates :difficulty, presence: true
 end
