d765185c709d8807ee4e1df1fe390539f50317da
Rails Course <muraokan@oregonstate.edu>
2019-12-24 12:01:26 +0900

    About page updated for potential customer and developer

--- db/migrate/20191211032003_create_users.rb
+++ db/migrate/20191211032003_create_users.rb
@@ -1,13 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
-  def change
-    create_table :users do |t|
-      t.string "username"
-      t.string "email"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.string "password_digest"
-      t.boolean "admin", default: false
-      t.timestamps
+    def change
+      create_table :users do |t|
+        t.string "username"
+        t.string "email"
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.string "password_digest"
+        t.boolean "admin", default: false
+        t.timestamps
+      end
     end
   end
-end
